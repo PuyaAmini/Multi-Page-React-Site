23b433c2c324b7fc5b23ed2374765f71b674cf53
pamini <puyagit@gmail.com>
2024-04-24 17:56:22 +0330

    React Router + subPages

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Contact() {
+       return (
+              <div>
+                     <h2>Contact</h2>
+                     <p>
+                     “Can’t be gabbin right now, I’m just a beginner still tryin to make sense of it all, ya know!”
+                     </p>
+              </div>
+       )
+}
