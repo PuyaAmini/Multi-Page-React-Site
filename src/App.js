50109a9ec5113c1e4a85cb40619b342d1d936418
pamini <puyagit@gmail.com>
2024-04-27 12:41:34 +0330

    Links & Nav Links + style for active pages nav  [S10 e4]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter, Route , Routes } from 'react-router-dom'
+import { BrowserRouter, Route , Routes  , Link, NavLink} from 'react-router-dom'
 
 import Home from './pages/Home'
 import About from './pages/About'
@@ -10,11 +10,18 @@ function App() {
   return (
     <div className="App">
 
-      <h1>Howya?</h1>
       <BrowserRouter>
+      <nav>
+        <h1>Howya?</h1>
+        {/* always use this when you navigating in ur React websites (Link & NavLink) instead od <a/> */}
+        {/* use '<a href  for external links*/}
+        <NavLink exact to='/'>Home</NavLink>
+        <NavLink to='/About'>About</NavLink>
+        <NavLink to='/Contact'>Contact</NavLink>
+        </nav>
         <nav>
           <Routes>
-            <Route path='/' element={<Home/>}/>
+            <Route path='/'  element={<Home/>}/>
             <Route path='/About' element={<About/>}/>
             <Route path='/Contact' element={<Contact/>}/>
           </Routes>
