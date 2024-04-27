ef6caf01cb2177d62549de18294c660c7602152f
pamini <puyagit@gmail.com>
2024-04-27 18:53:05 +0330

    The useParams Hook and display the fetched data [S10 e7]

--- src/App.js
+++ src/App.js
@@ -1,36 +1,34 @@
-import './App.css';
-import { BrowserRouter, Route , Routes  , Link, NavLink} from 'react-router-dom'
+import "./App.css";
+import { BrowserRouter, Route, Routes, Link, NavLink } from "react-router-dom";
 
-import Home from './pages/Home'
-import About from './pages/About'
-import Contact from './pages/Contact'
-import Article from './pages/Article';
-
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Article from "./pages/Article";
 
 function App() {
   return (
     <div className="App">
-
       <BrowserRouter>
-      <nav>
-        <h1>Howya?</h1>
-        {/* always use this when you navigating in ur React websites (Link & NavLink) instead od <a/> */}
-        {/* use '<a href  for external links*/}
-        <NavLink exact to='/'>Home</NavLink>
-        <NavLink to='/About'>About</NavLink>
-        <NavLink to='/Contact'>Contact</NavLink>
+        <nav>
+          <h1>Howya?</h1>
+          {/* always use this when you navigating in ur React websites (Link & NavLink) instead od <a/> */}
+          {/* use '<a href  for external links*/}
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+          <NavLink to="/About">About</NavLink>
+          <NavLink to="/Contact">Contact</NavLink>
         </nav>
 
         {/* <nav> */}
-          <Routes>
-
-            <Route path='/'  element={<Home/>}/>
-            <Route path='/About' element={<About/>}/>
-            <Route path='/Contact' element={<Contact/>}/>
-            <Route path='/articles/:id' element={<Article/>}/>
-            {/* ":" is Route Parameter and it say the id part is changeable */}
-            
-          </Routes>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+          <Route path="/articles/:id" element={<Article />} />
+          {/* ":" is Route Parameter and it say the id part is changeable */}
+        </Routes>
 
         {/* </nav> */}
       </BrowserRouter>
--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,8 +1,21 @@
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 export default function Article() {
+  const { id } = useParams();
+  const url = "http://localhost:3000/articles/" + id;
+  const { data: Article, isPending, error } = useFetch(url);
   return (
     <div>
-      Article Body
+      {isPending && <div> Loading ... </div>}
+      {error && <p>{error}</p>}
+      {Article && (
+        <div>
+          <h2>{Article.title}</h2>
+          <p>By {Article.author}</p>
+          <p>{Article.body}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
