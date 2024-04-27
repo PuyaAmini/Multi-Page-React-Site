29a60ec22787027942c8890880689895d3e613bb
pamini <puyagit@gmail.com>
2024-04-27 19:12:49 +0330

    Redirects (Programmatic Redirects) [S10 e8]

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,10 +1,20 @@
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import useFetch from "../hooks/useFetch";
+import { useEffect } from "react";
 
 export default function Article() {
   const { id } = useParams();
   const url = "http://localhost:3000/articles/" + id;
   const { data: Article, isPending, error } = useFetch(url);
+
+  const navigate  = useNavigate();
+  // If there's an error, navigate to the home page after displaying the error
+
+  useEffect(() =>{
+       if(error){
+              setTimeout(() => navigate('/') , 2000) //navigate(-1) = get back one page in history
+       }
+  } , [error , navigate])
   return (
     <div>
       {isPending && <div> Loading ... </div>}
