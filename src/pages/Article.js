8f200e60586fbff750ddfad94f53a54608671ff4
pamini <puyagit@gmail.com>
2024-04-28 11:37:55 +0330

    [*retrain] (useParams added ) Article-component updated by useFetch + useParams

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
 
 export default function Article() {
-  return (
-    <div>
-      article
-    </div>
-  )
+  const { id } = useParams();
+  const url = "http://localhost:3000/articles/" + id;
+  const { data: article, isPending, error } = useFetch(url);
+  return <React.Fragment>
+       {isPending && <React.Fragment>Loading...</React.Fragment>}
+       {error && <React.Fragment>{error}</React.Fragment>}
+       {article && (
+              <React.Fragment>
+                     <h2>{article.title}</h2>
+                     <p>{article.author}</p>
+                     <p>{article.body}</p>
+              </React.Fragment>
+       )}
+  </React.Fragment>;
 }
