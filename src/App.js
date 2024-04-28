016be8812d040df09cc395699bba118872bff51e
pamini <puyagit@gmail.com>
2024-04-28 08:20:25 +0330

    [*retrain] Routes + NavLinks #added + favicon #changed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 import "./App.css";
 
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+
 function App() {
+
   return (
     <div className="App">
-
+      <BrowserRouter>
+      <nav>
+      <h1>Howya?</h1>
+      <NavLink exact to={'/'}>Home</NavLink>
+      <NavLink to={'/Contact'}>Contact</NavLink>
+      <NavLink to={'/About'}>About</NavLink>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/About" element={<About/>}/>
+        <Route path="/Contact" element={<Contact/>}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <p>
+        To find a laptop wallpaper that doesn’t strain your eyes, you can search
+        for phrases like “eye-friendly laptop wallpapers,” “relaxing desktop
+        backgrounds,” or “calming wallpapers.” These searches should help you
+        discover soothing images suitable for your laptop’s background
+      </p>
+    </div>
+  );
+}
