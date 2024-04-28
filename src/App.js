871243b96a364066b07a62a9f5b67f816dceefd0
pamini <puyagit@gmail.com>
2024-04-28 07:41:54 +0330

    [*retrain] zero

--- src/App.js
+++ src/App.js
@@ -1,47 +1,9 @@
 import "./App.css";
-import {
-  BrowserRouter,
-  Route,
-  Routes,
-  NavLink,
-  Navigate,
-} from "react-router-dom";
-
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Article from "./pages/Article";
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <nav>
-          <h1>Howya?</h1>
-          {/* always use this when you navigating in ur React websites (Link & NavLink) instead od <a/> */}
-          {/* use '<a href  for external links*/}
-          <NavLink exact to="/">
-            Home
-          </NavLink>
-          <NavLink to="/About">About</NavLink>
-          <NavLink to="/Contact">Contact</NavLink>
-        </nav>
 
-        {/* <nav> */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Contact" element={<Contact />} />
-          <Route path="/articles/:id" element={<Article />} />
-          {/* ":" is Route Parameter and it say the id part is changeable */}
-
-          <Route path="*" element={<Navigate to="/" replace />} />
-          {/* Redirect any other path to the home page */}
-          {/* "replace" prop to prevent creating a new entry in the history stack */}
-        </Routes>
-
-        {/* </nav> */}
-      </BrowserRouter>
     </div>
   );
 }
